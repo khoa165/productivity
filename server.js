af31553d202370e2bd46bfcc5f135dcf7bf47147
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-05-24 05:41:14 -0500

    add helmet and limit rate for security

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const connectDB = require('./config/db');
 const app = express();
 
 // Connect database.
 connectDB();
 
+const limiter = new rateLimit({
+  windowMS: 15 * 60 * 100, // 15 minutes
+  max: 100, // limit of number of request per IP
+  delayMs: 100, // delay between 2 consecutive requests is 100 ms.
+});
+
 // Init middleware.
+app.use(helmet());
+app.use(limiter);
 app.use(express.json({ extended: false }));
 
 app.get('/', (_req, res) => res.send('API Running'));
