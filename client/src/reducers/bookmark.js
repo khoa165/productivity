34322f6d0ae9df39e7171a9835720fececaba710
Khoa Thien Le (Harry) <khoatle165@gmail.com>
2020-08-31 11:20:14 -0400

    add reducer for bookmark

---
+++ client/src/reducers/bookmark.js
@@ -0,0 +1,74 @@
+import {
+  GET_DEFAULT_BOOKMARKS,
+  ADD_NEW_DEFAULT_BOOKMARK,
+  UPDATE_BOOKMARK,
+  DELETE_BOOKMARK,
+  SET_CURRENT_EDITED_BOOKMARK,
+  CLEAR_CURRENT_EDITED_BOOKMARK,
+  ADD_NEW_BOOKMARK_PLACEHOLDER,
+  REMOVE_BOOKMARK_PLACEHOLDER,
+} from '../actions/types';
+
+const initialState = {
+  defaultBookmarks: [],
+  currentEditedBookmark: null,
+  loading: true,
+  bookmarkPlaceholders: [],
+};
+
+export default function (state = initialState, action) {
+  // Destructuring type and payload from action.
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_DEFAULT_BOOKMARKS:
+      return { ...state, loading: false, defaultBookmarks: payload };
+    case ADD_NEW_DEFAULT_BOOKMARK:
+      return {
+        ...state,
+        loading: false,
+        defaultBookmarks: [...state.defaultBookmarks, payload],
+      };
+    case UPDATE_BOOKMARK:
+      return {
+        ...state,
+        loading: false,
+        defaultBookmarks: state.defaultBookmarks.map((bookmark) =>
+          bookmark._id === payload._id ? payload : bookmark
+        ),
+      };
+    case DELETE_BOOKMARK:
+      return {
+        ...state,
+        loading: false,
+        defaultBookmarks: state.defaultBookmarks.filter(
+          (bookmark) => bookmark._id !== payload
+        ),
+      };
+    case SET_CURRENT_EDITED_BOOKMARK:
+      return {
+        ...state,
+        currentEditedBookmark: payload,
+      };
+    case CLEAR_CURRENT_EDITED_BOOKMARK:
+      return {
+        ...state,
+        currentEditedBookmark: null,
+      };
+    case ADD_NEW_BOOKMARK_PLACEHOLDER:
+      return {
+        ...state,
+        bookmarkPlaceholders: [...state.bookmarkPlaceholders, payload],
+      };
+    case REMOVE_BOOKMARK_PLACEHOLDER:
+      return {
+        ...state,
+        bookmarkPlaceholders: state.bookmarkPlaceholders.filter(
+          (placeholder) => placeholder !== payload
+        ),
+      };
+    default:
+      // Do nothing.
+      return state;
+  }
+}
